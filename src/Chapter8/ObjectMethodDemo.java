74e0d1147524720e6653136418811901a808d172
haenanim121@gmail.com <haenanim121@gmail.com>
2023-08-22 11:30:20 +0900

    first commit

---
+++ src/Chapter8/ObjectMethodDemo.java
@@ -0,0 +1,14 @@
+package Chapter8;
+
+public class ObjectMethodDemo {
+  public static void main(String[] args) {
+    Mouse mouse = new Mouse();
+    
+  }
+}
+
+class Mouse {
+  String name;
+
+}
+
